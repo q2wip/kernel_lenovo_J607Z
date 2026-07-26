d427bdca6f6c460f0685e67b228ba3f9a0e580b4
123 <no@thanks.com>
2026-07-26 19:15:21 +0800

    kona+lagoon: fix CS35L41 DAI (dai_name=cs35l41-pcm, MI2S, 4 devices)
    Based on PE reference analysis:
    - dai_name must be 'cs35l41-pcm' (CS35L41 driver registers DAI
      as 'cs35l41-pcm', not by I2C device name)
    - 4 CS35L41 devices at 0x40-0x43
    - Use MI2S (msm-dai-q6-mi2s.2), not TDM - remove
      CONFIG_TARGET_SUPPORT_TDM from Kbuild for ARCH_LAGOON

--- techpack/audio/asoc/kona.c
+++ techpack/audio/asoc/kona.c
@@ -90,31 +90,33 @@
 
 #define SWR_MAX_SLAVE_DEVICES 6
 
-//static atomic_t cs35l41_mclk_rsc_ref;
-
-#define CS35L41_SPEAKER_NAME "cs35l41.0-0040"
-#define CS35L41_RECEIVER_NAME "cs35l41.0-0041"
-struct snd_soc_dai_link_component cs35l41_codec_components[] = {
+static struct snd_soc_dai_link_component cs35l41_codec_components[] = {
 	{
-		.name = CS35L41_SPEAKER_NAME,
-		//.dai_name = "cs35l41-pcm",
-		.dai_name = CS35L41_SPEAKER_NAME,
+		.name = "cs35l41.3-0040",
+		.dai_name = "cs35l41-pcm",
 	},
 	{
-		.name = CS35L41_RECEIVER_NAME,
-		//.dai_name = "cs35l41-pcm",
-		.dai_name = CS35L41_RECEIVER_NAME,
+		.name = "cs35l41.3-0041",
+		.dai_name = "cs35l41-pcm",
+	},
+	{
+		.name = "cs35l41.3-0042",
+		.dai_name = "cs35l41-pcm",
+	},
+	{
+		.name = "cs35l41.3-0043",
+		.dai_name = "cs35l41-pcm",
 	},
 };
 
 static struct snd_soc_codec_conf cs35l41_codec_conf[] = {
 	{
-		.dev_name	= CS35L41_SPEAKER_NAME,
-		.name_prefix	= "SPK",
+		.dev_name	= "cs35l41.3-0040",
+		.name_prefix	= "SPK1",
 	},
 	{
-		.dev_name	= CS35L41_RECEIVER_NAME,
-		.name_prefix	= "RCV",
+		.dev_name	= "cs35l41.3-0041",
+		.name_prefix	= "SPK2",
 	},
 };
 
